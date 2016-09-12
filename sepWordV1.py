e7731d6d096cc1c4bdffc0977256eb095441fe24
Pear21 <abbot.pear@outlook.com>
2016-09-12 21:38:35 +0800

    finish 1st version

--- sepWordV1.py
+++ sepWordV1.py
@@ -1,20 +1,42 @@
 # -*- coding: utf-8 -*-
+import sys
 import jieba
 
-def segWord(rawStr, mode=False):
+def segWord(rawStr, mode = False):
     segList = jieba.cut(rawStr, cut_all=mode)
     return segList
 
-def countWord(segList):
-    freq = dict() 
+def countWord(segList, freq = dict()):
+    # freq = dict()
+    passWords = (u'，', u'。', u'、', u'的', u'和', u'在', u'了', u'是') # add 'u' for preventing UnicodeError
     for word in segList:
         # print word
-        freq[word] = freq.get(word, 0) + 1
-    return freq
+        if word not in passWords:
+            freq[word] = freq.get(word, 0) + 1
+    # return freq
 
+def main():
+    rawFile = sys.argv[1]
+    topNo = int(sys.argv[2])
+    with open(rawFile, 'r') as f:
+        wordFreq = dict()
+        for line in f.readlines():
+            line = line.strip()
+            segLine = segWord(line)
+            countWord(segLine, wordFreq)
+        sortedByValue = sorted(wordFreq, key = wordFreq.get, reverse = True)
+        for i in range(topNo):
+            word = sortedByValue[i]
+            print word, wordFreq[word]
+
+main()
+
+'''
 test = '有5个包裹，你明天最好带个大袋子，就放在饮水机旁边的桌子上。'
 tmp = segWord(test)
-# print '/'.join(tmp)
+print '/'.join(tmp)
 tmpFreq = countWord(tmp)
+
 for word, freq in tmpFreq.items():
     print word, freq
+'''
